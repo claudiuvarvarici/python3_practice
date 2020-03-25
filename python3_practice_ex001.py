6e627823ee2c6bc10059fcc22cbad458af710f6d
Claudiu Varvarici <47360403+claudiuvarvarici@users.noreply.github.com>
2020-03-25 16:45:40 +0100

    Create python3_practice_ex001.py

---
+++ python3_practice_ex001.py
@@ -0,0 +1,12 @@
+a="Twinkle, twinkle, little star,"
+b="How I wonder what you are!"
+c="Up above the world so high,"
+d="Like a diamond in the sky."
+tab="    "
+
+print(a)
+print(tab+b)
+print(tab+tab+c)
+print(tab+tab+d)
+print(a)
+print(tab+b)
